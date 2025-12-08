e7754e3941945a19a80a15c87915c453ffd71afd
Andrei80MLTDR <andreyy80@gmail.com>
2025-12-08 12:32:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ app = FastAPI()
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+from fastapi import FastAPI
+from app.routers import crypto, news, econ
+
+app = FastAPI()
+
+app.include_router(crypto.router)
+app.include_router(news.router)
+app.include_router(econ.router)
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
