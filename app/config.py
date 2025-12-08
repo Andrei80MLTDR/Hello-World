a4a8751f8272c23c37f5b6ff2e795753f101b1a6
Andrei80MLTDR <andreyy80@gmail.com>
2025-12-08 12:29:32 +0200

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,7 @@
+import os
+
+BINANCE_API_KEY = os.getenv("BINANCE_API_KEY", "")
+BINANCE_API_SECRET = os.getenv("BINANCE_API_SECRET", "")
+COINGECKO_API_KEY = os.getenv("COINGECKO_API_KEY", "")
+NEWSDATA_API_KEY = os.getenv("NEWSDATA_API_KEY", "")
+ECON_API_KEY = os.getenv("ECON_API_KEY", "")
